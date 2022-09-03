5b07df58cf0476c9ccd39ab4b8802167ae8d7060
Abbut <mailme.abbut@gmail.com>
2022-09-03 18:59:28 +0100

    NetworkLayer Added

---
+++ Currency-Converter/NetworkLayer/NetworkLayer/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Anumol Abraham Chakkungal on 25/08/2022.
+//
+
+import Foundation
+
+enum AppBaseUrl{
+    
+    static let stage = "v6.exchangerate-api.com/v6"
+}
---
+++ Currency-Converter/NetworkLayer/NetworkLayer/APIFeeds.swift
@@ -0,0 +1,14 @@
+//
+//  APIFeeds.swift
+//  WeatherApp
+//
+//  Created by Anumol Abraham Chakkungal on 27/08/2022.
+//
+
+import Foundation
+
+public enum ApiFeeds {
+    
+    case getConversionRate
+    
+}
